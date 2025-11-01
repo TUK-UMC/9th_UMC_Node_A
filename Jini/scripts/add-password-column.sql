029fc6e7a36ff6498a51c516579e67dc4d1f723e
2coconut <wosjiwjs@gmail.com>
2025-11-01 22:37:04 +0900

    feat: 불필요 파일 삭제

--- Jini/scripts/add-password-column.sql
+++
@@ -1,5 +0,0 @@
--- User 테이블에 password 컬럼 추가
-ALTER TABLE User ADD COLUMN password VARCHAR(255) AFTER phone_number;
-
--- 기존 사용자들에게 임시 비밀번호 설정 (선택사항)
--- UPDATE User SET password = 'temp_password' WHERE password IS NULL;
